cb708288936766fc63707d414f3c79676b84aa5f
Sebastian Perez <sperezm97@gmail.com>
2020-02-02 18:22:02 -0400

    Create Blank Screen MyGarbage

---
+++ src/MyGarbage/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function MyGarbage() {
+  return (
+    <View>
+      <Text>MyGarbage</Text>
+    </View>
+  );
+}
