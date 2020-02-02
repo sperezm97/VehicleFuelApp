516e0e7f581511eb9f2b7e72391ecdb24fb5993a
Sebastian Perez <sperezm97@gmail.com>
2020-02-02 19:43:39 -0400

    Export global screens

---
+++ src/app/screens.ts
@@ -0,0 +1,6 @@
+import Fuel from "../Fuel";
+import MyGarbage from "../MyGarbage";
+import Reports from "../Reports";
+import Services from "../Services";
+
+export { Fuel, MyGarbage, Reports, Services };
