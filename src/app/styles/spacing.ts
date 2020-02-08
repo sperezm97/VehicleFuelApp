2dcaca1d2b613335c342501c15056a2e13f8773c
Sebastian Perez <sperezm97@gmail.com>
2020-02-08 16:45:17 -0400

    Add Device Width and Device height poprs

--- src/app/styles/spacing.ts
+++ src/app/styles/spacing.ts
@@ -1,3 +1,5 @@
+import { Dimensions } from "react-native";
+
 export default {
   none: 0,
   extraTight: 4,
@@ -5,5 +7,8 @@ export default {
   baseTight: 12,
   base: 16,
   loose: 20,
-  extraLoose: 32
+  extraLoose: 32,
+
+  deviceWidth: Dimensions.get("window").width,
+  deviceHeight: Dimensions.get("window").height
 };
