9619bc1de2672b05fd58e046901658d2750c7f2e
Sebastian Perez <sperezm97@gmail.com>
2020-02-08 16:42:32 -0400

    Create Header List Component

---
+++ src/app/components/List/HeaderList.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import View from "../View";
+import Text from "../Text";
+import Picker from "../Picker";
+import colors from "../../styles/colors";
+import IHeaderList from "../../types/HeaderList";
+
+const HeaderList: React.FunctionComponent<IHeaderList> = props => {
+  return (
+    <View headerContainer>
+      <View headerList>
+        <Text body color={colors.grey.base}>
+          Showing fuel data for:
+        </Text>
+        <Picker
+          selectedValue={props.value}
+          onValueChange={props.handleValueChange}
+          options={props.options}
+          placeholder={props.placeholder}
+        />
+      </View>
+      <View bigLine />
+    </View>
+  );
+};
+
+export default HeaderList;
