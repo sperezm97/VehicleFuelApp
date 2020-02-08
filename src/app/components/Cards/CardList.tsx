73366ee57caaaa84a9ac4c0942362db8560a3956
Sebastian Perez <sperezm97@gmail.com>
2020-02-08 16:44:20 -0400

    Create List Container Component

---
+++ src/app/components/Cards/CardList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import View from "../View";
+import BaseList from "../List/BaseList";
+import GallonItem from "../List/GallonItem";
+import IHeaderList from "../../types/HeaderList";
+
+interface CardList {
+  data: [any];
+  renderItem: () => React.ReactElement;
+  header: React.ReactElement<IHeaderList>;
+}
+
+const CardList: React.FunctionComponent<CardList> = props => {
+  return (
+    <View card="container">
+      <BaseList
+        data={props.data}
+        renderItem={props.renderItem}
+        header={props.header}
+      />
+    </View>
+  );
+};
+
+export default CardList;
