20ca8d1a3ad4779b9e0d648aba9b338e018b39b5
Sebastian Perez <sperezm97@gmail.com>
2020-02-08 16:44:57 -0400

    indexes Component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,25 @@
 import Text from "./Text";
+import View from "./View";
+import Container from "./Container";
 
-export { Text };
+import CardList from "./Cards/CardList";
+import RightCard from "./Cards/RightCard";
+import LeftCard from "./Cards/LeftCard";
+
+import BaseList from "./List/BaseList";
+import GallonItem from "./List/GallonItem";
+import HeaderList from "./List/HeaderList";
+import ServiceItem from "./List/ServiceItem";
+
+export {
+  Text,
+  View,
+  CardList,
+  RightCard,
+  LeftCard,
+  Container,
+  BaseList,
+  GallonItem,
+  HeaderList,
+  ServiceItem
+};
