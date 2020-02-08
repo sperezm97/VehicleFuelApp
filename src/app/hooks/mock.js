cc4d78c9444df0f8a7747068a050ca7ddacf0f25
Sebastian Perez <sperezm97@gmail.com>
2020-02-08 16:48:32 -0400

    Create mock.js

---
+++ src/app/hooks/mock.js
@@ -0,0 +1,72 @@
+export const fuelList = [
+  {
+    id: 1,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  },
+  {
+    id: 2,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  },
+  {
+    id: 3,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  },
+  {
+    id: 4,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  },
+  {
+    id: 5,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  },
+  {
+    id: 6,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  },
+  {
+    id: 7,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  },
+  {
+    id: 8,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  },
+  {
+    id: 9,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  },
+  {
+    id: 10,
+    gallonConsumed: "9.95",
+    galloTotalPricer: "42.909",
+    milagePerGallon: "24.21",
+    dateFully: "2018/06/21"
+  }
+];
