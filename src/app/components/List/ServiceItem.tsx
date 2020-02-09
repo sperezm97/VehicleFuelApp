752992f6b28410d0b735e033991d7f3615662572
Sebastian Perez <sperezm97@gmail.com>
2020-02-09 11:47:11 -0400

    Add Styles to Service Item

--- src/app/components/List/ServiceItem.tsx
+++ src/app/components/List/ServiceItem.tsx
@@ -1,28 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { ListItem, Body, Left, Right } from "native-base";
+import { ListItem } from "native-base";
+import Text from "../Text";
+import View from "../View";
+import colors from "../../styles/colors";
 
 interface ServiceItem {}
 const ServiceItem = () => {
   return (
-    <ListItem>
-      <Left>
-        <View>
-          <Text>M</Text>
+    <ListItem noIndent noBorder style={{ justifyContent: "space-between" }}>
+      <View row>
+        <View center serviceThumb color={colors.purple}>
+          <Text heading color={colors.white}>
+            M
+          </Text>
         </View>
-      </Left>
-      <Body>
-        <View>
-          <Text>oil Change</Text>
-          <Text>Maintenance</Text>
+        <View style={{ paddingLeft: 15, alignItems: "flex-start" }}>
+          <Text displaySmall color={colors.black} weight={600}>
+            Oil Change
+          </Text>
+          <Text subHeading color={colors.grey.base} weight={400}>
+            Maintenance
+          </Text>
         </View>
-      </Body>
-      <Right>
-        <View>
-          <Text>$520.00</Text>
-          <Text>Jun 15, 18</Text>
-        </View>
-      </Right>
+      </View>
+      <View>
+        <Text displaySmall color={colors.red} weight={500}>
+          $42.909
+        </Text>
+        <Text body color={colors.grey.base}>
+          Jun 21, 18
+        </Text>
+      </View>
     </ListItem>
   );
 };
