1a679fae89c981aed4f69b4b1a5da7f87742883a
Sebastian Perez <sperezm97@gmail.com>
2020-02-09 11:47:22 -0400

    Complete Service Screen

--- src/Services/index.tsx
+++ src/Services/index.tsx
@@ -1,12 +1,54 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
 import withHOC from "../app/hooks/hoc";
-
+import {
+  Container,
+  View,
+  LeftCard,
+  RightCard,
+  CardList,
+  FloatButton,
+  HeaderList,
+  ServiceItem
+} from "../app/components";
+import { Content } from "native-base";
+import { serviceList } from "../app/hooks/mock";
 const Services: React.FunctionComponent = () => {
+  const [value, setValue] = useState("All Vehicles");
+
+  const header = () => (
+    <HeaderList
+      value={value}
+      handleValueChange={setValue}
+      placeholder="All Vehicles"
+      options={[{ label: "nissa sentra", value: 1 }]}
+    />
+  );
+
+  const renderItem = () => {
+    return <ServiceItem />;
+  };
+
   return (
-    <View>
-      <Text>Services</Text>
-    </View>
+    <Container>
+      <View row style={{ marginTop: 20 }}>
+        <View flex>
+          <LeftCard />
+        </View>
+        <View flex>
+          <RightCard />
+        </View>
+      </View>
+      <Content
+        contentContainerStyle={{ flex: 1 }}
+        scrollEnabled={false}
+        nestedScrollEnabled
+      >
+        <CardList data={serviceList} renderItem={renderItem} header={header} />
+      </Content>
+      <View>
+        <FloatButton onPress={() => {}} />
+      </View>
+    </Container>
   );
 };
 export default withHOC(Services);
