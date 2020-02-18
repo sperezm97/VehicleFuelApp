daa95669658aaa5066db5b66f47245fbc627a2c9
Sebastian Perez <sperezm97@gmail.com>
2020-02-18 11:05:27 -0400

    Create rootReduces.ts

---
+++ src/features/rootReduces.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import fuelSlice from "./Fuel/fuelSlice";
+import myGarageSlice from "./MyGarage/myGarageSlice";
+
+export default combineReducers({
+  fuels: fuelSlice.reducer,
+  myGarage: myGarageSlice.reducer
+});
