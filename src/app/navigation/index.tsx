2b60d1a723f0e5b702fd019fb1b2048af1174bb1
Sebastian Perez <sperezm97@gmail.com>
2020-02-18 11:06:15 -0400

    Add shorhand

--- src/app/navigation/index.tsx
+++ src/app/navigation/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import MainTapNavigator from "./tabs";
+import { WithSafeArea, WithTheme, WithProvider } from "../../comon/hooks/hoc";
 
 export default function AppNavigator() {
   return (
-    <NavigationContainer>
-      <MainTapNavigator />
-    </NavigationContainer>
+    <WithSafeArea>
+      <WithTheme>
+        <WithProvider>
+          <NavigationContainer>
+            <MainTapNavigator />
+          </NavigationContainer>
+        </WithProvider>
+      </WithTheme>
+    </WithSafeArea>
   );
 }
